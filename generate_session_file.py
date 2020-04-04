599de2eedc4b9507d164e21373a48abfb13f52d7
bs9935189 <63019591+bs9935189@users.noreply.github.com>
2020-04-05 04:34:23 +0700

    Update generate_session_file.py

--- generate_session_file.py
+++ generate_session_file.py
@@ -11,8 +11,8 @@ import os
 
 load_dotenv("config.env")
 
-API_KEY = os.environ.get("API_KEY", None)
-API_HASH = os.environ.get("API_HASH", None)
+API_KEY = os.environ.get("API_KEY", 1309461)
+API_HASH = os.environ.get("API_HASH", 4e3c7ed9288d1e2b45fc75723e2a9484)
 
 bot = TelegramClient('userbot', API_KEY, API_HASH)
 bot.start()
